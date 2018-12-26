db8b0813c9ef23bcf0b2a49d4b9499a56379f7c4
Vikas.aga <vikas.agarwal@mountblue.io>
2018-12-26 19:02:03 +0530

    added combined reducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,42 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { combineReducers, createStore } from 'redux';
+
+
+const userReducer = ( state={name:"will",age:27}, action)=>{
+    
+    if(action.type==='CHANGE_USERNAME'){
+      state = {...state, name:action.payload}
+    }
+    else if(action.type==='CHANGE_AGE'){
+      state = {...state, age:action.payload}
+    }
+    return state;
+}
+
+const tweetReducer = (state=["hi"],action)=>{
+    
+    if(action.type==='CHANGE_USERNAME'){
+        state = [...state, action.payload];
+       }
+    return state;     //each reducer should returns state
+}
+const reducers = combineReducers({
+    user:userReducer,
+    tweets:tweetReducer
+});
+
+const store   = createStore(reducers); // 0 initial store
+store.subscribe(()=>{   // to listen to the store changes
+    console.log('store changed ', store.getState());
+})
+
+store.dispatch({type:'CHANGE_USERNAME',payload:"will"}); // type is mandatory 
+store.dispatch({type:'CHANGE_AGE',payload:35});
+store.dispatch({type:'CHANGE_USERNAME',payload:"Vikas"});
+store.dispatch({type:'CHANGE_USERNAME',payload:"vikki"});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
